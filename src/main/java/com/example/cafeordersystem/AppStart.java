96cac590799094b436d24225e4c6715165a94150
haarooo <pshb200137@gmail.com>
2026-06-05 16:12:21 +0900

    초기 커밋 생성

---
+++ src/main/java/com/example/cafeordersystem/AppStart.java
@@ -0,0 +1,11 @@
+package com.example.cafeordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+    }
+}
