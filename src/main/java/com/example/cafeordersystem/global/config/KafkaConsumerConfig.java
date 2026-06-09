36f825c8dc550fd64c6f674546796f3db786179c
haarooo <pshb200137@gmail.com>
2026-06-09 12:38:00 +0900

    kafka 조건부 설정

--- src/main/java/com/example/cafeordersystem/global/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/cafeordersystem/global/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@ConditionalOnProperty(name = "app.kafka.enabled", havingValue = "true")
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
